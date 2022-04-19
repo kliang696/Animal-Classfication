c28ddaa1a1abdb0dc3a850115413ff7b3d714572
kliang696 <kliang696@gwu.edu>
2022-04-18 21:07:25 -0400

    data aug,droup out

--- tensor_flow.py
+++ tensor_flow.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import warnings
 import numpy as np
 import os
 import PIL
@@ -7,6 +8,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+
+from tensorflow.keras.applications.resnet50 import ResNet50
 import pathlib
 #-----------------------
 OR_PATH = os.getcwd()
@@ -21,11 +24,24 @@ random_seed = 42
 train_size=0.8
 
 batch_size = 5
-img_height = 180
-img_width = 180
+img_height = 256
+img_width = 256
+
+
+
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  DATA_DIR,
+  directory=DATA_DIR,
   validation_split=0.2,
   subset="training",
   seed=42,
@@ -33,36 +49,32 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   batch_size=batch_size)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  DATA_DIR,
+  directory=DATA_DIR,
   validation_split=0.2,
   subset="validation",
-  seed=123,
+  seed=42,
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
 
-class_names = train_ds.class_names
-print(class_names)
-
-num_classes = len(class_names)
-
 model = Sequential([
+  data_augmentation,
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-epochs=5
+epochs=10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
@@ -70,3 +82,28 @@ history = model.fit(
 )
 
 loss, accuracy = model.evaluate(val_ds)
+
+
+
+###plot
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
