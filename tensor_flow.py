1cb0169dde6c9fb7c6e695f9d15a9fe131ea5c83
kliang696 <kliang696@gwu.edu>
2022-04-19 18:38:32 -0400

    tensorflow

--- tensor_flow.py
+++ tensor_flow.py
@@ -55,10 +55,11 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   seed=42,
   image_size=(img_height, img_width),
   batch_size=batch_size)
-
-
+class_names = train_ds.class_names
+num_classes = len(class_names)
 model = Sequential([
   data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -68,6 +69,7 @@ model = Sequential([
   layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
